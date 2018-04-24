36c6e0e403069c85dee7266a2ed7e7e5305c1263
bahrus <anderson.bruce.b@gmail.com>
2018-04-23 21:59:45 -0400

    checkbox

---
+++ xtal-material-checkbox-radio.js
@@ -0,0 +1,32 @@
+import { XtalMaterialInput } from './xtal-material-input.js';
+import { initCE } from './xtal-shadow.js';
+export class XtalMaterialCheckbox extends XtalMaterialInput {
+    static get is() { return 'xtal-material-checkbox'; }
+    getType() {
+        return 'checkbox';
+    }
+    get checked() {
+        return this._inputElement.checked;
+    }
+    set checked(val) {
+        this._inputElement.checked = val;
+    }
+    emitEvent() {
+        const newEvent = new CustomEvent('checked-changed', {
+            detail: {
+                value: this._inputElement.checked
+            },
+            bubbles: true,
+            composed: false
+        });
+        this.dispatchEvent(newEvent);
+    }
+    addInputListener() {
+        //some browsers don't support 'input' change on checkbox yet
+        this._inputElement.addEventListener('change', e => {
+            this.emitEvent();
+        });
+    }
+}
+initCE(XtalMaterialCheckbox.is, XtalMaterialCheckbox);
+//# sourceMappingURL=xtal-material-checkbox-radio.js.map
