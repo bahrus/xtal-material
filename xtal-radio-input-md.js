5c66e2fe51279c8cc62d8b3d715bfcc1882d9d76
bahrus <anderson.bruce.b@gmail.com>
2018-04-29 21:11:32 -0400

    ...

--- xtal-radio-input-md.js
+++
@@ -1,7 +0,0 @@
-import { XtalTextInputMD } from './text-input/xtal-text-input-md.js';
-import { initCE, basePath } from './bra-ket.js';
-export class XtalRadioInputMD extends XtalTextInputMD {
-    static get is() { return 'xtal-radio-input-md'; }
-}
-initCE(XtalRadioInputMD.is, XtalRadioInputMD, basePath);
-//# sourceMappingURL=xtal-radio-input-md.js.map
