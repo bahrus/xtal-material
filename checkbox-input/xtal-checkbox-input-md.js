cc21c9505f62263430d1c51544806b4f7ac62639
bahrus <anderson.bruce.b@gmail.com>
2018-04-29 20:13:22 -0400

    Move into folders

--- checkbox-input/xtal-checkbox-input-md.js
+++ checkbox-input/xtal-checkbox-input-md.js
@@ -1,19 +1,16 @@
-import {XtalTextInputMD} from './xtal-text-input-md.js';
-import {initCE, basePath} from './bra-ket.js';
-
-export class XtalCheckboxInputMD extends XtalTextInputMD{
+import { XtalTextInputMD } from '../xtal-text-input-md.js';
+import { initCE, basePath } from '../bra-ket.js';
+export class XtalCheckboxInputMD extends XtalTextInputMD {
     static get is() { return 'xtal-checkbox-input-md'; }
     // getType() {
     //     return 'checkbox';
     // }
-
     get checked() {
         return this._inputElement.checked;
     }
     set checked(val) {
         this._inputElement.checked = val;
     }
-
     emitEvent() {
         const newEvent = new CustomEvent('checked-changed', {
             detail: {
@@ -21,15 +18,15 @@ export class XtalCheckboxInputMD extends XtalTextInputMD{
             },
             bubbles: true,
             composed: false
-        } as CustomEventInit);
+        });
         this.dispatchEvent(newEvent);
     }
-
-    addInputListener(){
+    addInputListener() {
         //some browsers don't support 'input' change on checkbox yet
         this._inputElement.addEventListener('change', e => {
-            this.emitEvent()
+            this.emitEvent();
         });
     }
 }
-initCE(XtalCheckboxInputMD.is, XtalCheckboxInputMD, basePath);
+initCE(XtalCheckboxInputMD.is, XtalCheckboxInputMD, basePath + '/checkbox-input');
+//# sourceMappingURL=xtal-checkbox-input-md.js.map
---
+++ checkbox-input/xtal-checkbox-input-md.ts
@@ -0,0 +1,35 @@
+import {XtalTextInputMD} from '../xtal-text-input-md.js';
+import {initCE, basePath} from '../bra-ket.js';
+
+export class XtalCheckboxInputMD extends XtalTextInputMD{
+    static get is() { return 'xtal-checkbox-input-md'; }
+    // getType() {
+    //     return 'checkbox';
+    // }
+
+    get checked() {
+        return this._inputElement.checked;
+    }
+    set checked(val) {
+        this._inputElement.checked = val;
+    }
+
+    emitEvent() {
+        const newEvent = new CustomEvent('checked-changed', {
+            detail: {
+                value: this._inputElement.checked
+            },
+            bubbles: true,
+            composed: false
+        } as CustomEventInit);
+        this.dispatchEvent(newEvent);
+    }
+
+    addInputListener(){
+        //some browsers don't support 'input' change on checkbox yet
+        this._inputElement.addEventListener('change', e => {
+            this.emitEvent()
+        });
+    }
+}
+initCE(XtalCheckboxInputMD.is, XtalCheckboxInputMD, basePath + '/checkbox-input');
