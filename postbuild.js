d8c00c449353113310ffcbba3518bea023cbed56
bahrus <anderson.bruce.b@gmail.com>
2018-06-05 13:27:13 -0400

    ...

--- postbuild.js
+++ postbuild.js
@@ -1,6 +1,6 @@
 //@ts-check
 const jiife = require('jiife');
-jiife.processFiles(['getBasePath.js', 'node_modules/templ-mount/templ-mount.js', 'node_modules/xtal-latx/xtal-latx.js', 
+jiife.processFiles(['getBasePath.js', 'node_modules/templ-mount/first-templ.js', 'node_modules/templ-mount/templ-mount.js', 'node_modules/xtal-latx/xtal-latx.js', 
     'node_modules/bra-ket/bra-ket.js', 'adopt-a-child.js', 'text-input/xtal-text-input-md.js', 
     'checkbox-input/xtal-checkbox-input-md.js', 'radio-group/xtal-radio-group-md.js', 'radio-tabs/xtal-radio-tabs-md.js', 'text-area/xtal-text-area-md.js'], 'xtal-material.js');
 
--- xtal-material.js
+++ xtal-material.js
@@ -20,6 +20,48 @@
 function lispToSnakeCase(s) {
     return s.split('-').join('_');
 }
+const _cachedTemplates = {};
+const fetchInProgress = {};
+function loadTemplate(template, params) {
+    const src = template.dataset.src;
+    if (src) {
+        if (_cachedTemplates[src]) {
+            template.innerHTML = _cachedTemplates[src];
+            if (params)
+                customElements.define(params.tagName, params.cls);
+        }
+        else {
+            if (fetchInProgress[src]) {
+                if (params) {
+                    setTimeout(() => {
+                        loadTemplate(template, params);
+                    }, 100);
+                }
+                return;
+            }
+            fetchInProgress[src] = true;
+            fetch(src, {
+                credentials: 'same-origin'
+            }).then(resp => {
+                resp.text().then(txt => {
+                    fetchInProgress[src] = false;
+                    if (params && params.preProcessor)
+                        txt = params.preProcessor.process(txt);
+                    _cachedTemplates[src] = txt;
+                    template.innerHTML = txt;
+                    template.setAttribute('loaded', '');
+                    if (params)
+                        customElements.define(params.tagName, params.cls);
+                });
+            });
+        }
+    }
+    else {
+        if (params && params.tagName)
+            customElements.define(params.tagName, params.cls);
+    }
+}
+//# sourceMappingURL=first-templ.js.map
 // const _cachedTemplates : {[key:string] : string} = {};
 // const fetchInProgress : {[key:string] : boolean} = {};
 function qsa(css, from) {
