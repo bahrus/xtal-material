69ca9f5d011f940b9fbffa08eedcb18373180447
bahrus <anderson.bruce.b@gmail.com>
2018-09-15 06:58:10 -0400

    import meta working now

--- getBasePath.ts
+++ getBasePath.ts
@@ -3,19 +3,17 @@ export function getBasePath(tagName) {
     const link = self['xtal_material'];
     if (link) {
         path = link.href;
-    }
-    else {
-        const cs = document.currentScript;
+    } else {
+        const cs = document.currentScript as HTMLScriptElement;
         if (cs) {
             path = cs.src;
-        }
-        else {
+        } else {
             path = import.meta['url'];
         }
     }
     return path.split('/').slice(0, -1).join('/');
 }
+
 export function lispToSnakeCase(s) {
     return s.split('-').join('_');
 }
-//# sourceMappingURL=getBasePath.js.map
