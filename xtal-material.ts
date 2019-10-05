8b60bd4de7343f0085684707122a4b86c3c65633
bahrus <anderson.bruce.b@gmail.com>
2019-10-05 19:16:14 -0400

    ...

--- xtal-material.ts
+++ xtal-material.ts
@@ -4,3 +4,5 @@ import 'xtal-text-input-md/xtal-autocomplete-decorator.js';
 import 'xtal-text-area-md/xtal-text-area-md.js';
 import 'xtal-radio-group-md/xtal-radio-group-md.js';
 import 'xtal-checkbox-input-md/xtal-checkbox-input-md.js';
+import './xtal-radio-tabs-md.js';
+import './xtal-side-nav.js';
