fb167fa5d7236da3e33642a39ecab192f0492a27
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-03-29 16:19:46 -0400

    ...

--- createIIFE2.js
+++
@@ -1,9 +0,0 @@
-//@ts-check
-const tiife = require('jiife/tiffe');
-const nm = 'node_modules/';
-const xe = nm + 'xtal-element/';
-const es = nm + 'event-switch/';
-const tr = nm + 'trans-render/';
-tiife.processFiles([xe + 'define.ts', xe + 'xtal-latx.ts',  xe + 'xtal-element.ts', xe + 'utils.ts',
-    es + 'event-switch.d.ts', es + 'event-switch.ts', tr + 'init.d.ts', tr + 'init.ts', 'xtal-text-input-md.ts'
-], 'dist/iife.ts');
